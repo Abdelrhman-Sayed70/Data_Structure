2a8d19a5db5450d41d0397c58dfa63ed3b36b740
Abdelrhman Sayed <99830416+Abdelrhman-Sayed70@users.noreply.github.com>
2022-03-01 18:06:50 +0200

    Update 2 pointers part 1 .cpp

--- Sources/Pointers/2 pointers part 1 .cpp
+++ Sources/Pointers/2 pointers part 1 .cpp
@@ -8,6 +8,7 @@ int main() {
 	p2 = &v2;
 	cout << *p1 << " " << *p2 << "\n";  // 8 9 
 	// make p1 point to what p2 point
+	// put the address of p2 to p1 
 	p1 = p2;
 	cout << *p1 << " " << *p2 << "\n";  // 9 9 
 	
